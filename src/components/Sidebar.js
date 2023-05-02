04723b1170010fc59b6d54a8d2038bac479a812f
Caleb Flanagan <caleb.flanagan@clearlink.com>
2023-05-02 10:57:42 -0600

    adding sidebar links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import styled from '@emotion/styled';
 
 import theme from '../styles/theme';
@@ -18,12 +19,21 @@ const SidebarContainer = styled.aside`
   }
 
   ul {
-    list-style: none;
     padding-left: 0;
 
     li {
-      margin-bottom: 10px;
-      font-size: 18px;
+      margin-bottom: 25px;
+
+      a {
+        color: ${theme.colors.darkGray};
+        font-family: ${theme.fonts.robotoCondensed};
+        text-decoration: none;
+
+        &:hover {
+          color: ${theme.colors.tan};
+          text-decoration: underline;
+        }
+      }
     }
   }
 `;
@@ -33,10 +43,17 @@ const Sidebar = () => {
     <SidebarContainer>
       <h3>Bonus Articles</h3>
       <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
+        <li><Link href="/what-i-read-in-march-2023">What I Read in March 2023</Link></li>
+        <li><Link href="">How Many Times Have You Read Your Favorite Book?</Link></li>
+        <li><Link href="">Which Anti-Hero Do You Love?</Link></li>
+        <li><Link href="">How I Feel About the Wheel of Time TV Show</Link></li>
+        <li><Link href="">Kindle, Print, Audiobook: How Do You Read?</Link></li>
+        <li><Link href="">My Constant Battle with Genre Fiction Hating Standalones</Link></li>
+        <li><Link href="">Best Science Fiction Books I Read in 2022</Link></li>
+        <li><Link href="">New TBR Additions: Lady Astronauts & Devil Dinosaurs</Link></li>
+        <li><Link href="">You'll Never Read Everything: I Promise It's Okay</Link></li>
+        <li><Link href="">How Many Books Are On Your TBR?</Link></li>
+        <li><Link href="">What I Read in February 2023</Link></li>
       </ul>
     </SidebarContainer>
   );
