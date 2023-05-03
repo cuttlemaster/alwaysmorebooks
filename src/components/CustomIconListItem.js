22d2d371da6781f51671d1de38e3ddfb1876ddba
Caleb Flanagan <caleb.flanagan@clearlink.com>
2023-05-03 16:35:07 -0600

    fix icon image import

--- src/components/CustomIconListItem.js
+++ src/components/CustomIconListItem.js
@@ -2,11 +2,34 @@ import React from 'react';
 import Link from 'next/link';
 import styled from '@emotion/styled';
 
+import theme from '../styles/theme';
+
 import AwardStarIcon from '../images/icons/award-star.svg';
-import EditeNoteIcon from '../images/icons/edit-note.svg';
+import EditNoteIcon from '../images/icons/edit-note.svg';
 import FolderIcon from '../images/icons/folder.svg';
 
-const StyledIconListItem = styled.li``;
+const StyledIconListItem = styled.li`
+  list-style-type: none;
+  padding-left: 30px;
+  background-repeat: no-repeat;
+  background-position: left center;
+  margin-bottom: 25px;
+
+  &:award-star-icon { background-image: url(${AwardStarIcon}); }
+  &:edit-note-icon { background-image: url(${EditNoteIcon}); }
+  &:folder-icon { background-image: url(${FolderIcon}); }
+
+  a {
+    color: ${theme.colors.darkGray};
+    font-family: ${theme.fonts.robotoCondensed};
+    text-decoration: none;
+
+    &:hover {
+      color: ${theme.colors.tan};
+      text-decoration: underline;
+    }
+  }
+`;
 
 const CustomIconListItem = ({ icon, href, children }) => {
   let iconClass = '';
